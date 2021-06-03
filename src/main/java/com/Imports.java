c003bfe643eceb859cdecf127562cb0c29844180
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2021-06-03 14:52:18 +0300

    optimize old and new imports in file with "Modified" status

--- src/main/java/com/Imports.java
+++ src/main/java/com/Imports.java
@@ -1,11 +1,7 @@
 package com;
 
-import java.util.Map;
-import java.util.ArrayList;
-import java.awt.color.ColorSpace;
-import java.util.List;
-
 public class Imports {
 
     //change
+    //change for commit
 }
