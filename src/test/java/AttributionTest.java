3529309f1f00a4d041f2e88a5115295eb9564a8e
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2021-06-09 12:31:30 +0300

    Test added

--- src/test/java/AttributionTest.java
+++ src/test/java/AttributionTest.java
@@ -1,12 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AttributionTest {
 
+    @BeforeEach
+    public void waitASecond() throws InterruptedException {
+        Thread.sleep(1000);
+    }
+
     @Test
     public void test() {
         assertTrue(false);
     }
-
 }
