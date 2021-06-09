3995a61839e65015736f76515816bbe1646b3ca2
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2021-06-09 12:31:30 +0300

    Test added

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,8 +1,14 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
+
+    @BeforeEach
+    public void waitASecond() throws InterruptedException {
+        Thread.sleep(1000);
+    }
 
     @Test
     public void test() {
