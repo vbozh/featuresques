516d1df1398d635ee2d339cc326a77726d7f1149
Vika <victoria.bozhko@gmail.com>
2021-06-17 13:56:33 +0300

    TestFile moved

---
+++ src/test/java/packageMove/AttributionTest.java
@@ -0,0 +1,15 @@
+package packageMove;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AttributionTest {
+
+    @Test
+    public void test() {
+        assertTrue(false);
+    }
+
+    //todo
+}
