4925c4eae4fa05d34b9d2d182b73a3dfc4460b36
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2021-06-17 16:59:47 +0300

    Added AttributionTest.java back to test package

---
+++ src/test/java/AttributionTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AttributionTest {
+
+    @Test
+    public void test() {
+        assertTrue(false);
+    }
+
+    //todo
+
+    public String change(String added_lines) {
+        return added_lines;
+    }
+}
