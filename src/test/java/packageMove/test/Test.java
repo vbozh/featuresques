3ac33cabe9600c9a66df8e0933a88b32f85a0daf
Vika <viktoria.bozhko@jetbrains.com>
2021-11-15 18:21:17 +0300

    changes in Test!

--- src/test/java/packageMove/test/Test.java
+++ src/test/java/packageMove/test/Test.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 
 public class Test {
     @org.junit.jupiter.api.Test
-    public void vvv_333() {
+    public void vvv_444() {
         Assertions.assertTrue(true);
     }
 
