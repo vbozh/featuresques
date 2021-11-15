b284342439634ce7480e4a1209a29f5e71292a61
Vika <viktoria.bozhko@jetbrains.com>
2021-11-15 14:17:06 +0300

    rename in new changes!

--- src/main/java/NewChanges.java
+++ src/main/java/NewChanges.java
@@ -1,6 +1,6 @@
 public class NewChanges {
 
     public void foo() {
-        for (int i = 0; i < 10; i++) System.out.println(i);
+        for (int renamed_i = 0; renamed_i < 10; renamed_i++) System.out.println(renamed_i);
     }
 }
