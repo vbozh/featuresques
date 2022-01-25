847428d19b2821921f17c0b613c436900f28dc83
Vika <victoria.bozhko@gmail.com>
2022-01-25 14:27:17 +0300

    change for upstr

--- src/main/java/NewChanges.java
+++ src/main/java/NewChanges.java
@@ -3,4 +3,8 @@ public class NewChanges {
     public void foo() {
         for (int renamed_i = 0; renamed_i < 10; renamed_i++) System.out.println(renamed_i);
     }
+
+    public void bar() {
+        System.out.println("olololo!k");
+    }
 }
